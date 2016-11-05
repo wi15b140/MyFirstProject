af518fd5b972f68291b47c962ffaf37c159a61a8
wi15b140 <wi15b140@technikum-wien.at>
2016-11-05 20:49:59 +0100

    write numbers from 1 to 6

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,6 +10,12 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            int[] numbers = { 1, 2, 3, 4, 5, 6 };
+
+            for(int i = 0; i <= numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
         }
     }
 }
